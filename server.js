64887cc2070c5df841e792400f3d70b7ad9a54a2
samki6576 <hismm8690@email.com>
2026-07-06 16:50:20 +0500

    add

--- server.js
+++
@@ -1,8 +0,0 @@
-﻿const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 5000;
-
-app.get('/', (req, res) => res.json({ message: 'Hello from Pxxl!' }));
-app.get('/health', (req, res) => res.status(200).json({ status: 'OK' }));
-
-app.listen(PORT, '0.0.0.0', () => console.log('Server running on port ' + PORT));
