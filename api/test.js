2999c91b95377b71dbbf4ff391c6667f18956535
samki6576 <hismm8690@email.com>
2026-07-11 19:22:30 +0500

    Add test endpoint for debugging

---
+++ api/test.js
@@ -0,0 +1,9 @@
+﻿module.exports = (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.status(200).json({ 
+        message: 'Test endpoint is working!',
+        method: req.method,
+        path: req.url,
+        headers: req.headers
+    });
+};
